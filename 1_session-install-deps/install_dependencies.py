da3140ae0737988bfd6def45e8df375f1f7344d8
Vinod KC <vinod.kc.in@gmail.com>
2023-08-16 19:46:20 -0700

    Update install_dependencies.py

--- 1_session-install-deps/install_dependencies.py
+++ 1_session-install-deps/install_dependencies.py
@@ -1,2 +1,2 @@
-!pip install --upgrade pip
+!pip install --upgrade pip setuptools==59.8.0 wheel
 !pip install --no-cache-dir --log 1_session-install-deps/pip-req.log -r 1_session-install-deps/requirements.txt
